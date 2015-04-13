6eff336fd49689e677b72014112610985a1ded8d
balajisoundar <balajisoundar@users.noreply.github.com>
2015-04-13 12:07:02 +0530

    Update rwd.js

--- javascripts/rwd.js
+++ javascripts/rwd.js
@@ -7,7 +7,13 @@ function rwd()
 			{
 				var rect=ele[i].getBoundingClientRect();
 				if(rect.right>window.innerWidth)
+				{
 					ele[i].style.border="2px solid red";
+					var text=document.createElement("span");
+					text.innerHTML=window.innerWidth+"px";
+					text.style.color="red";
+					ele[i].appendChild(text);
+				}
 			}
 		}
 		
