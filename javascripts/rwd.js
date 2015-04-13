b7af66d9e8386e081254c5cf4b85d883f0eae498
balajisoundar <balajisoundar@users.noreply.github.com>
2015-04-13 11:52:39 +0530

    Create rwd.js

---
+++ javascripts/rwd.js
@@ -0,0 +1,14 @@
+window.addEventListener("load",function()
+	{
+		var ele=document.querySelectorAll("*");
+		for(var i=0;i<ele.length;i++)
+		{
+			if(ele[i].nodeType==1)
+			{
+				var rect=ele[i].getBoundingClientRect();
+				if(rect.right>window.innerWidth)
+					ele[i].style.border="2px solid red";
+			}
+		}
+		
+	});
